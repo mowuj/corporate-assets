c328c5d598ae3e3de655786fdfcb466624aee2ec
mowuj <mowuj92@gmail.com>
2023-04-13 01:38:31 +0600

    company and employee model create

--- track/models.py
+++ track/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Company(models.Model):
+    name=models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+class employee(models.Model):
+    name = models.CharField(max_length = 150)
+    designation = models.CharField(max_length = 150)
+    join_date = models.DateField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    
